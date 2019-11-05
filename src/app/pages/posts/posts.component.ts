a8c742511a9c0214189ca8300f2b2b542d9de6cb
Miguel Angel Ochoa <miguel.ochoa@AlphaCredit.local>
2019-11-05 16:53:13 -0600

    first commit

---
+++ src/app/pages/posts/posts.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  messages: any;
+
+  constructor( private dataService: DataService) { }
+
+  ngOnInit() {
+    this.messages = this.dataService.getPosts();
+
+//    .subscribe( (posts: any[]) => {
+//      console.log(posts);
+//      this.messages = posts;
+//    });
+
+  }
+
+}
